0bd5be683d968d02360c5f7902bb43885db158ad
Javier Salcedo <javiersalcedo.p@gmail.com>
2025-06-01 19:31:24 +0100

    Add `init`, `build`, `run` and `test` commands
    `run` and `test` remain unimplemented, and `build` only builds the
    project config.

--- dedalo.cpp
+++ dedalo.cpp
@@ -59,6 +59,23 @@ private:
 #define defer( f ) const auto CONCATENATE( _deferred, __COUNTER__ ) = Deferrable( [&](){ f; } );
 
 
+#define UNIMPLEMENTED() printf( "UNIMPLEMENTED!\n" )
+
+
+enum [[nodiscard]] ResultCode
+{
+    UNKNOWN_ERROR       = -1,
+    OK                  = 0,
+    // CLI errors
+    INVALID_ARGUMENT    = EINVAL,
+    INVALID_TARGET      = ELAST + 1,
+    INVALID_GENERATOR,
+    ALREADY_INITIALIZED,
+    BUILD_SCRIPT_LOAD_FAILED,
+
+};
+
+
 enum struct Compiler: u8 { Clang, GCC, MSVC };
 
 
@@ -99,8 +116,6 @@ struct Dependency
 
 struct Project
 {
-    fun build() {};
-
     enum Type: u8 { Executable, Library };
     struct Args
     {
@@ -213,59 +228,158 @@ using BuildCfgFunPtr = void(*)(Project*);
 BuildCfgFunPtr build_cfg = nullptr;
 
 
-fun main( i32 argc, char* argv[] ) -> i32
+fun init() -> ResultCode
 {
+    if( fs::is_regular_file( "build.cpp" ) )
+    {
+        printf( "Project already initialized. Skipping.\n" );
+        return ALREADY_INITIALIZED;
+    }
+
     // Create the directory structure
     {
-        fs::create_directory( "vendor" );
+        fs::create_directory( "dependencies" );
         fs::create_directory( "tests"  );
         fs::create_directory( "build"  );
-        fs::create_directory( "src"    );
+        {
+            fs::create_directory( "build/bin"  );
+            fs::create_directory( "build/src"  );
+            fs::create_directory( "build/dependencies"  );
+        }
 
-        if( !fs::is_regular_file( "src/main.cpp" ) )
+        fs::create_directory( "src" );
         {
             var* main_file = fopen( "src/main.cpp", "w" );
             assert( main_file );
             fwrite( new_main_file_template.c_str(), sizeof(char), new_main_file_template.length(), main_file );
             fclose( main_file );
         }
-
-        if( !fs::is_regular_file( "build.cpp" ) )
-        {
-            var* build_file = fopen( "build.cpp", "w" );
-            assert( build_file );
-
-            // Replace the project's name with the current folder's
-            let current_folder_name = fs::current_path().stem().string();
-            var filled_template = new_project_template;
-            filled_template.replace(
-                filled_template.find( name_tag ),
-                name_tag.length(),
-                current_folder_name );
-
-            fwrite( filled_template.c_str(), sizeof(char), filled_template.length(), build_file );
-            fclose( build_file );
-        }
     }
 
+    // Init build.cpp
+    {
+        var* build_file = fopen( "build.cpp", "w" );
+        assert( build_file );
+
+        // Replace the project's name with the current folder's
+        let current_folder_name = fs::current_path().stem().string();
+        var filled_template = new_project_template;
+        filled_template.replace(
+            filled_template.find( name_tag ),
+            name_tag.length(),
+            current_folder_name );
+
+        fwrite( filled_template.c_str(), sizeof(char), filled_template.length(), build_file );
+        fclose( build_file );
+    }
+    return OK;
+}
+
+
+fun build( const Project& project ) -> ResultCode
+{
+    printf( "Starting build of project \"%s\"...\n", project.name.c_str() );
+    defer( printf( "...Done.\n" ) );
+
+    UNIMPLEMENTED();
+
+    // TODO: Fetch any remote dependencies and place them in dependencies/
+    // TODO: Compile each file in src/ and dependencies/ into .o in their corresponding directories in build/
+    // TODO: Link any dynamic libraries into their corresponding .so in build/bin/
+    // TODO: Link everything into the executable in build/bin/
+
+    return OK;
+}
+
+
+fun build() -> ResultCode
+{
     // Compile the build.cpp
     {
         // TODO: Check the timestamp to determine if it needs to be compiled to begin with
         // TODO: Dynamically choose the compiler
-        system( "clang++ -fPIC -shared -Wall -Werror -O3 -o ./build/build_script.so build.cpp" );
+        if( let error_code = system( "clang++ -fPIC -shared -Wall -Werror -O3 -o ./build/build_script.so build.cpp" ) )
+        {
+            printf( "Build script compilation failed with error %d.\n", error_code );
+            return BUILD_SCRIPT_LOAD_FAILED;
+        }
     }
 
     // Load the build( Project* ) symbol
     {
         var* dll = dlopen( "./build/build_script.so", RTLD_NOW );
-        assert( dll );
+        if( !dll )
+        {
+            printf( "ERROR: Couldn't load build script's DLL.\n" );
+            return BUILD_SCRIPT_LOAD_FAILED;
+        }
         build_cfg = (BuildCfgFunPtr) dlsym(dll, "build");
-        assert( build_cfg );
-        // No need to close the dll
+        if( !build_cfg )
+        {
+            printf( "ERROR: Couldn't load build function's symbol.\n" );
+            return BUILD_SCRIPT_LOAD_FAILED;
+        }
+        // No need to close the dll, the OS will clean up after us.
     }
 
-    Project project_cfg;
-    build_cfg( &project_cfg );
+    var project = Project();
+    build_cfg( &project );
+    return build( project );
+}
+
+
+fun run() -> ResultCode
+{
+    // TODO:
+    UNIMPLEMENTED();
+    return OK;
+}
+
+
+fun test() -> ResultCode
+{
+    // TODO:
+    UNIMPLEMENTED();
+    return OK;
+}
+
+
+fun main( i32 argc, char* argv[] ) -> i32
+{
+    if( argc < 2 )
+    {
+        printf( "No command provided.\n" );
+        return INVALID_ARGUMENT;
+    }
+
+    let cmd = String( argv[1] );
+    if( cmd == "init" )
+    {
+        return init();
+    }
+    else if( cmd == "build" )
+    {
+        return build();
+    }
+    else if( cmd == "run" )
+    {
+        var build_result = build();
+        if( build_result == OK )
+        {
+            build_result = run();
+        }
+        return build_result;
+    }
+    else if( cmd == "test" )
+    {
+        return test();
+    }
+    else
+    {
+        printf( "ERROR: Unrecognized command %s.\n", cmd.c_str() );
+        return INVALID_ARGUMENT;
+    }
+    return OK;
 }
 
 #endif // !INCLUDE_AS_HEADER
